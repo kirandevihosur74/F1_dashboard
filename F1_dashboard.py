c3460eeecf31b4664172d69dcb3ed79fca1116f6
Kiran Devihosur <144872359+kirandevihosur74@users.noreply.github.com>
2025-03-31 20:34:44 -0600

    Update F1_dashboard.py

--- F1_dashboard.py
+++ F1_dashboard.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("🏎️ Formula 1 Insights Dashboard")
-
-st.markdown("""
-<iframe src="https://public.tableau.com/views/F1_dashboard_new/Dashboard2?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
-        width="1200" height="1000" frameborder="0"></iframe>
-""", unsafe_allow_html=True)
-
+st.markdown(
+    """
+    <iframe src="https://public.tableau.com/views/F1_dashboard_new/Dashboard2?:embed=y&:display_count=yes&:showVizHome=no"
+            width="1400" height="1400" frameborder="0" style="border:none;"></iframe>
+    """,
+    unsafe_allow_html=True
+)
